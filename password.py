74e7f1fd3315ffa21e560c1ad7300d369712bf3d
S <guerreqq@gmail.com>
2023-01-24 02:00:17 +0800

    update verson_3

--- password.py
+++ password.py
@@ -1,10 +1,14 @@
 password = 'a123456' #先定義正解
 i = 3
 while i > 0:
+	i = i - 1
 	pwd = input('請輸入密碼')
 	if pwd == password:
 		print('登入成功')
 		break
 	else:
-		i = i - 1
-		print('輸入錯誤 還有', i,'次機會')
+		print('輸入錯誤')
+		if i > 0:
+			print('還有', i,'次機會')
+		else:
+			print('沒有機會嘗試囉')
